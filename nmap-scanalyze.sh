80813b43263237d89f28fe524ca043984826ff5a
zruvv <88829709+zruvv@users.noreply.github.com>
2021-08-20 13:29:20 -0400

    Update and rename nmap-analyzeoutput.sh to nmap-scanalyze.sh

--- nmap-scanalyze.sh
+++ nmap-scanalyze.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "Enter path to nmap output files (exclude the trailing /): "
+
+##################################
+#ANALYZING OUTPUT OF SCAN RESULTS#
+##################################
+
+echo "Enter file path to nmap output files (exclude the trailing /): "
 read path
 echo "Anlayzing Nmap output"
 
@@ -14,10 +19,10 @@ egrep -v "^#|Status: Up" $udp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//
 egrep -v "^#|Status: Up" $udp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk -F, '{split($0,a," "); printf "Host: %-20s UDP Ports Open: %d\n" , a[1], NF}' | sort -k 6 -r | uniq
 
 echo -e "\n\e[1;32mAll Active host + TCP ports/service\e[0m"
-egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }' > hosts_open_tcp.txt
+egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }' > activehosts_open_tcp.txt
 egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }'
 
-echo -e "\n\e[1;33mFull results can be found under $path/xxx_hosts_top_open.txt\e[0m"
+echo -e "\n\e[1;33mFull results can be found under $path\e[0m"
 
 echo -e "\nWould you like to move the raw nmap output files to a new sub dir - nmap_raw (y/n): "
 read answer
@@ -25,12 +30,12 @@ echo $answer
 if [ "$answer" = "y" ];
 then
 	mkdir $path/nmap_raw 2>/dev/null
-	mv *discovery.gnmap $path/nmap_raw -f 2>/dev/null
-	mv *discovery.nmap $path/nmap_raw -f 2>/dev/null
-	mv *discovery.xml $path/nmap_raw -f 2>/dev/null
-	mv *pscan.gnmap $path/nmap_raw -f 2>/dev/null
-	mv *pscan.nmap $path/nmap_raw -f 2>/dev/null
-	mv *pscan.xml $path/nmap_raw -f 2>/dev/null
+	mv $path/*discovery.gnmap $path/nmap_raw/ #-f 2>/dev/null
+	mv $path/*discovery.nmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*discovery.xml $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.gnmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.nmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.xml $path/nmap_raw/ -f 2>/dev/null
 	echo "Files were moved";
 else
 	echo "Files were not moved";
