fa6e3cf8ce4e3c1f73fceba41e7c5974bf5ec2f8
zruvv <88829709+zruvv@users.noreply.github.com>
2021-08-20 13:20:11 -0400

    Create nmap-int-scan_analyze.sh

---
+++ nmap-int-scan_analyze.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+####################
+#STARTING NMAP SCAN#
+####################
+
+echo -e "Run as sudo...\n *This script is suitable for small - mid size internal scopes* \n"
+echo "Enter file path to internalscope.txt file (exclude the trailing /): "
+echo "Ex: /home/iv4user/Desktop/pentest/client/internal/scope"
+read path
+
+echo -e "\nEnter in name of internal scope file: "
+echo "Ex: internalscope.txt"
+read scope
+
+echo -e "\n\e[1;32mStarting Host Discovery Scan \e[0m"
+nmap -n -sn -T4 -iL $path/$scope -oA $path/hostdiscovery
+
+echo -e "\n\e[1;33mCreating activehosts.txt file \e[0m"
+cat $path/hostdiscovery.gnmap | awk '/Up$/{print $2}' > $path/activehosts.txt
+
+echo -e "\n\e[1;32mStarting TCP Scan \e[0m"
+nmap -n -sS -Pn -T4 -iL $path/activehosts.txt -oA $path/tcpscan
+
+echo -e "\n\e[1;32mStarting UDP Scan \e[0m"
+nmap -n -sU -Pn -iL $path/activehosts.txt -p 53,67-68,88,110-111,161-162,414,500-501 --reason -oA $path/udpscan
+
+echo -e "\e[1;33mScans have completed, output files are located in $path\n\e[0m"
+
+##################################
+#ANALYZING OUTPUT OF SCAN RESULTS#
+##################################
+
+echo "Anlayzing Nmap output"
+
+tcp_file=$path/tcpscan.gnmap
+udp_file=$path/udpscan.gnmap
+echo -e "\e[1;32mOpen TCP ports\e[0m"
+egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk -F, '{split($0,a," "); printf "Host: %-20s TCP Ports Open: %d\n" , a[1], NF}' | sort -k 6 -r | uniq > $path/tcp_hosts_top_open.txt
+egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk -F, '{split($0,a," "); printf "Host: %-20s TCP Ports Open: %d\n" , a[1], NF}' | sort -k 6 -r | uniq
+
+echo -e "\n\e[1;32mOpen UDP ports\e[0m"
+egrep -v "^#|Status: Up" $udp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk -F, '{split($0,a," "); printf "Host: %-20s UDP Ports Open: %d\n" , a[1], NF}' | sort -k 6 -r | uniq > $path/udp_hosts_top_open.txt
+egrep -v "^#|Status: Up" $udp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk -F, '{split($0,a," "); printf "Host: %-20s UDP Ports Open: %d\n" , a[1], NF}' | sort -k 6 -r | uniq
+
+echo -e "\n\e[1;32mAll Active host + TCP ports/service\e[0m"
+egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }' > $path/activehosts_open_tcp.txt
+egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }'
+
+echo -e "\n\e[1;33mFull results can be found under $path\e[0m"
+
+echo -e "\nWould you like to move the raw nmap output files to a new sub dir - nmap_raw (y/n): "
+read answer
+echo $answer
+if [ "$answer" = "y" ];
+then
+	mkdir $path/nmap_raw 2>/dev/null
+	mv $path/*discovery.gnmap $path/nmap_raw/ #-f 2>/dev/null
+	mv $path/*discovery.nmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*discovery.xml $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.gnmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.nmap $path/nmap_raw/ -f 2>/dev/null
+	mv $path/*pscan.xml $path/nmap_raw/ -f 2>/dev/null
+	echo "Files were moved";
+else
+	echo "Files were not moved";
+fi
