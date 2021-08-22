2a498fa89943587cdebff79860cb8d8ba0290cc1
zruvv <88829709+zruvv@users.noreply.github.com>
2021-08-22 09:23:21 -0400

    Update nmap-scanalyze.sh

--- nmap-scanalyze.sh
+++ nmap-scanalyze.sh
@@ -20,13 +20,14 @@ echo -e "\n\e[1;32mAll Active host + TCP ports/service\e[0m"
 egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }' > $path/hosts_open_tcp.txt
 egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }'
 
+mkdir $path/hosts_ports
 cat $tcp_file | awk '/80\/open/ {print $2}' > hosts_80.txt
 cat $tcp_file | awk '/22\/open/ {print $2}' > hosts_22.txt
 cat $tcp_file | awk '/445\/open/ {print $2}' > hosts_445.txt
 cat $tcp_file | awk '/21\/open/ {print $2}' > hosts_21.txt
 cat $tcp_file | awk '/23\/open/ {print $2}' > hosts_23.txt
 cat $tcp_file | awk '/3389\/open/ {print $2}' > hosts_3389.txt
-
+find $path -type f -size 0 -delete
 
 echo -e "\n\e[1;33mFull results can be found under $path\e[0m"
 
@@ -36,7 +37,7 @@ echo $answer
 if [ "$answer" = "y" ];
 then
 	mkdir $path/nmap_raw 2>/dev/null
-	mv $path/*discovery.gnmap $path/nmap_raw/ #-f 2>/dev/null
+	mv $path/*discovery.gnmap $path/nmap_raw/ -f 2>/dev/null
 	mv $path/*discovery.nmap $path/nmap_raw/ -f 2>/dev/null
 	mv $path/*discovery.xml $path/nmap_raw/ -f 2>/dev/null
 	mv $path/*pscan.gnmap $path/nmap_raw/ -f 2>/dev/null
