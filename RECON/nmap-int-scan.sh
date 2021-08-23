66fccfc1abe471421a7714ce055a55908e1bf30e
zruvv <88829709+zruvv@users.noreply.github.com>
2021-08-23 18:33:04 -0400

    Update nmap-int-scan.sh

--- RECON/nmap-int-scan.sh
+++ RECON/nmap-int-scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "Run as sudo...\n *This script is suitable for small - mid size internal scopes* \n"
 echo "Enter file path to internalscope.txt file (exclude the trailing /): "
-echo "Ex: /home/iv4user/Desktop/pentest/client/internal/scope"
+echo "Ex: /home/user/Desktop/pentest/client/internal/scope"
 read path
 
 echo -e "\nEnter in name of internal scope file: "
