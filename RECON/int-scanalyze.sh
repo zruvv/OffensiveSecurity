e979701650ae02aed34a1a04131e7fcf2773bf7b
zruvv <88829709+zruvv@users.noreply.github.com>
2021-08-24 06:01:18 -0400

    Update and rename nmap-int-scanalyze.sh to int-scanalyze.sh

--- RECON/int-scanalyze.sh
+++ RECON/int-scanalyze.sh
@@ -80,3 +80,27 @@ then
 else
 	echo "Files were not moved";
 fi
+
+#################
+#SMB ENUMERATION#
+#################
+
+echo -e "\nWould you like to enumerate SMB hosts (y/n): "
+read smb_answer
+if [ "$smb_answer" = "y" ];
+then
+	nmap -sU -sS -Pn -n -iL $path/hosts_445.txt --script smb-vuln-ms17-010,smb-os-discovery,smb-protocols,smb-enum-shares -p T:445,T:139,U:137 --open -oN smb_details.nmap
+	smbhosts=$(cat $path/hosts_445.txt)
+	
+	#Checks for null session access, SMB shares + permissions
+	for ip in $smbhosts
+	do
+		echo -e "\n\e[1;32mSMB results for $ip\e[0m" 
+		rpcclient -U "" $ip
+		# smbclient -L \\\\$ip -N
+		smbmap -H $ip
+		# enum4linux $ip
+	done
+else
+	echo "Not enumerating SMB hosts"
+fi
