f2f53c7a21d76e7c1cae1b375ecea981c821873a
Michael Wurz <mwurz@proarch.com>
2021-09-01 07:32:41 -0400

    Update activate-the-director.sh

--- RECON/activate-the-director.sh
+++ RECON/activate-the-director.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Enter in the IP Address of the target Domain Controller: "
-read dc_ip
-
+read -p "Enter in the IP Address of the target Domain Controller: " dc_ip
+read -p  "Would you like to perform User Enumeration using Kerbrute? (y/n): " answer
+read -p "Please choose one of the following options on username format: john.doe, j.doe, johndoe, custom: " list
+if [ "$list" = "custom" ]; then
+	read -p "please enter wordlist file location " wordlist 
+fi
 #Collect DC domain (domain.local)
 string="domain:"
 domain=$(crackmapexec smb $dc_ip | awk '{print $12}' | sed 's/.*(\(.*\))/\1/' | sed -e "s/^$string//")
@@ -19,18 +22,32 @@ echo -e "Checking for Anonymous RPC Access"
 echo -e " run enumdomains, enumdomusers, enumdomgroups, enumalsgroups, enumdata, exit"
 #rpcclient $dc_ip -U '' -N
 
+kerpath=$(locate kerbrute.py)
 
-echo -e "Would you like to start User Enumeration using Kerbrute? (y/n)"
-#read answer
-
-
-if [ "$answer" = "y" ]; then
-    kerpath=$(find / -name kerbrut.py 2>/dev/null);
-    if [ $kerpath == *"kerbrute.py"* ]; then
-        echo -e "kerbrute.py was found: $kerpath"
-        python3 $kerpath -domain $domain -users /home/iv4user/Desktop/pentest/tools/resources/wordlists/usernames_john.doe.txt -dc-ip $dc_ip -outputusers kerbrute_users.txt;
-    else
-        echo -e "kerbrute.py was NOT found, exiting kerbrute loop";
+if [[ "$answer" = "y" && "$kerpath" = *"kerbrute.py"* ]]; then
+	echo -e "kerbrute.py was found: $kerpath"
+	echo -e "$wordlist was selected"
+	case $list in
+		john.doe)
+			echo -n "john.doe selected "
+			wordlist="/usr/share/wordlists/custom/usernames/john.doe.txt"
+			;;
+		j.doe)
+			echo -n "j.doe selected "
+			wordlist="/usr/share/wordlists/custom/usernames/j.doe.txt"
+			;;
+		jdoe)
+			echo -n "jdoe selected "
+			wordlist="/usr/share/wordlists/custom/usernames/jdoe.txt"
+			;;
+		custom)
+			echo -n "custom selected"
+			;;
+            	*)		
+			echo -n "Error: unkown selection"
+			;;		
+	    esac
+        python3 $kerpath -domain $domain -users $wordlist -dc-ip $dc_ip -outputusers kerbrute_users.txt
 else
-    echo -e "Input did not equal y, exiting kerbrute loop";
+        echo -e "kerbrute.py was NOT found or input did not equal y, exiting kerbrute loop"
 fi
