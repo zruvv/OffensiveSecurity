a0ccd3bbec0ad3a77c439485a549eaaf99b11126
zruvv <88829709+zruvv@users.noreply.github.com>
2021-10-18 08:48:18 -0400

    Update domain-recon.sh

--- RECON/domain-recon.sh
+++ RECON/domain-recon.sh
@@ -2,18 +2,23 @@
 echo "Enter in target domain: "
 read domain
 
+echo "DNS Lookup"
 dnsrecon -d $domain > dns_records.txt
+echo "Whois Lookup"
 whois $domain > whois_records_raw.txt
 
 #Collect publicly available email addresses
+echo "Collecting publicly available email addresses using theHarvester"
 theHarvester -d $domain -b google > harvester_results.txt
 theHarvester -d $domain -b duckduckgo >> harvester_results.txt
 theHarvester -d $domain -b yahoo >> harvester_results.txt
 theHarvester -d $domain -b bing >> harvester_results.txt
+echo "Completed email collection"
 
 #Extract emails addresses into a seperate file
 var_domain=@$domain
 cat harvester_results.txt | awk -v var="$var_domain"  '$0~var{print}' > email.txt
 
 #Search for Domain Typos
+echo "Searching for Domain Typos using URLCrazy"
 urlcrazy --format=CSV $domain > domaintypo_results.csv
