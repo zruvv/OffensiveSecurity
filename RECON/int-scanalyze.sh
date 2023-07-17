18a74cbc7a2723e0f31b1acf7b43632db10f300e
zruvv <88829709+zruvv@users.noreply.github.com>
2023-07-17 11:45:26 -0400

    Update int-scanalyze.sh
    Removed user input on moving files.

--- RECON/int-scanalyze.sh
+++ RECON/int-scanalyze.sh
@@ -48,38 +48,29 @@ egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//
 egrep -v "^#|Status: Up" $tcp_file | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }'
 
 mkdir $path/hosts_ports
-cat $tcp_file | awk '/80\/open/ {print $2}' > hosts_80.txt
-cat $tcp_file | awk '/443\/open/ {print $2}' > hosts_443.txt
-cat $tcp_file | awk '/8080\/open/ {print $2}' > hosts_8080.txt
-cat $tcp_file | awk '/22\/open/ {print $2}' > hosts_22.txt
-cat $tcp_file | awk '/445\/open/ {print $2}' > hosts_445.txt
-cat $tcp_file | awk '/21\/open/ {print $2}' > hosts_21.txt
-cat $tcp_file | awk '/23\/open/ {print $2}' > hosts_23.txt
-cat $tcp_file | awk '/25\/open/ {print $2}' > hosts_25.txt
-cat $tcp_file | awk '/69\/open/ {print $2}' > hosts_69.txt
-cat $tcp_file | awk '/1433\/open/ {print $2}' > hosts_1433.txt
-cat $tcp_file | awk '/3389\/open/ {print $2}' > hosts_3389.txt
-cat $tcp_file | awk '/3306\/open/ {print $2}' > hosts_3306.txt
+cat $tcp_file | awk '/80\/open/ {print $2}' > $path/hosts_ports/hosts_80.txt
+cat $tcp_file | awk '/443\/open/ {print $2}' > $path/hosts_ports/hosts_443.txt
+cat $tcp_file | awk '/8080\/open/ {print $2}' > $path/hosts_ports/hosts_8080.txt
+cat $tcp_file | awk '/22\/open/ {print $2}' > $path/hosts_ports/hosts_22.txt
+cat $tcp_file | awk '/445\/open/ {print $2}' > $path/hosts_ports/hosts_445.txt
+cat $tcp_file | awk '/21\/open/ {print $2}' > $path/hosts_ports/hosts_21.txt
+cat $tcp_file | awk '/23\/open/ {print $2}' > $path/hosts_ports/hosts_23.txt
+cat $tcp_file | awk '/25\/open/ {print $2}' > $path/hosts_ports/hosts_25.txt
+cat $tcp_file | awk '/69\/open/ {print $2}' > $path/hosts_ports/hosts_69.txt
+cat $tcp_file | awk '/1433\/open/ {print $2}' > $path/hosts_ports/hosts_1433.txt
+cat $tcp_file | awk '/3389\/open/ {print $2}' > $path/hosts_ports/hosts_3389.txt
+cat $tcp_file | awk '/3306\/open/ {print $2}' > $path/hosts_ports/hosts_3306.txt
 find $path -type f -size 0 -delete
 
 echo -e "\n\e[1;33mFull results can be found under $path\e[0m"
 
-echo -e "\nWould you like to move the raw nmap output files to a new sub dir - nmap_raw (y/n): "
-read answer
-echo $answer
-if [ "$answer" = "y" ];
-then
-	mkdir $path/nmap_raw 2>/dev/null
-	mv $path/*discovery.gnmap $path/nmap_raw/ -f 2>/dev/null
-	mv $path/*discovery.nmap $path/nmap_raw/ -f 2>/dev/null
-	mv $path/*discovery.xml $path/nmap_raw/ -f 2>/dev/null
-	mv $path/*pscan.gnmap $path/nmap_raw/ -f 2>/dev/null
-	mv $path/*pscan.nmap $path/nmap_raw/ -f 2>/dev/null
-	mv $path/*pscan.xml $path/nmap_raw/ -f 2>/dev/null
-	echo "Files were moved";
-else
-	echo "Files were not moved";
-fi
+mkdir $path/nmap_raw 2>/dev/null
+mv $path/*discovery.gnmap $path/nmap_raw/ -f 2>/dev/null
+mv $path/*discovery.nmap $path/nmap_raw/ -f 2>/dev/null
+mv $path/*discovery.xml $path/nmap_raw/ -f 2>/dev/null
+mv $path/*pscan.gnmap $path/nmap_raw/ -f 2>/dev/null
+mv $path/*pscan.nmap $path/nmap_raw/ -f 2>/dev/null
+mv $path/*pscan.xml $path/nmap_raw/ -f 2>/dev/null
 
 #################
 #SMB ENUMERATION#
